86b3736e17dd602128347313398d7504a6b8d328
Rose Sharon Anchi <rosesharonanchi123@gmail.com>
2024-08-07 10:28:25 +0100

    update

--- js/myscript.js
+++ js/myscript.js
@@ -2,7 +2,10 @@ let submit = document.querySelector("#submit");
 let cards = document.querySelectorAll(".card");
 let total = document.querySelector("#total");
 let orderWrapper = document.querySelector(".order-container");
-let deleteButton = document.querySelectorAll(".delete-icon");
+
+let noItem = document.querySelector(".no-order");
+let orderMade = document.querySelector(".order-made");
+let myOrder = document.querySelectorAll(".my-order");
 
 // ... rest of the product objects
 let products = [
@@ -13,16 +16,16 @@ let products = [
     quantity: 0,
     imageDesktop: "assets/images/image-waffle-thumbnail.jpg",
     price: 6.5,
-    totalPrice: 0
+    totalPrice: 0,
   },
   {
     id: 1,
-    title: "Waffle",
-    description: "Waffles with Berries",
+    title: "Creme Brulee",
+    description: "Vanilla Bean Creme Brulee",
     quantity: 0,
     imageDesktop: "assets/images/image-waffle-thumbnail.jpg",
     price: 6.5,
-    totalPrice: 0
+    totalPrice: 0,
   },
   {
     id: 2,
@@ -30,68 +33,76 @@ let products = [
     description: "Waffles with Berries",
     quantity: 0,
     imageDesktop: "assets/images/image-waffle-thumbnail.jpg",
-    price: 6.5,
-    totalPrice: 0
+    price: 7.00,
+    totalPrice: 0,
   },
   {
     id: 3,
-    title: "Waffle",
-    description: "Waffles with Berries",
+    title: "Macaron",
+    description: "Macaron Mix of Five",
     quantity: 0,
     imageDesktop: "assets/images/image-waffle-thumbnail.jpg",
-    price: 6.5,
-    totalPrice: 0
+    price: 8.00,
+    totalPrice: 0,
   },
   {
     id: 4,
-    title: "Waffle",
-    description: "Waffles with Berries",
+    title: "Tiramisu",
+    description: "Classic Tiramisu",
     quantity: 0,
     imageDesktop: "assets/images/image-waffle-thumbnail.jpg",
-    price: 6.5,
-    totalPrice: 0
+    price: 5.50,
+    totalPrice: 0,
   },
   {
     id: 5,
-    title: "Waffle",
-    description: "Waffles with Berries",
+    title: "Baklava",
+    
+    description: "Pistachio Baklava",
     quantity: 0,
     imageDesktop: "assets/images/image-waffle-thumbnail.jpg",
-    price: 6.5,
-    totalPrice: 0
+    price: 4.00,
+    totalPrice: 0,
   },
+  
   {
     id: 6,
-    title: "Waffle",
-    description: "Waffles with Berries",
+    title: "Pie",
+    description: "Lemon Meringue Pie",
     quantity: 0,
     imageDesktop: "assets/images/image-waffle-thumbnail.jpg",
-    price: 6.5,
-    totalPrice: 0
+    price: 5.00,
+    totalPrice: 0,
   },
   {
     id: 7,
-    title: "Waffle",
-    description: "Waffles with Berries",
+    title: "Cake",
+    description: "Red Velvet Cake",
     quantity: 0,
     imageDesktop: "assets/images/image-waffle-thumbnail.jpg",
-    price: 6.5,
-    totalPrice: 0
+    price: 4.5,
+    totalPrice: 0,
   },
   {
     id: 8,
-    title: "Waffle",
-    description: "Waffles with Berries",
+    title: "Brownie",
+    description: "Salted Caramel Brownie",
     quantity: 0,
     imageDesktop: "assets/images/image-waffle-thumbnail.jpg",
-    price: 6.5,
-    totalPrice: 0
+    price: 5.50,
+    totalPrice: 0,
   },
-  
 ];
 let orders = [];
+let updateTotal = () => {
+  let myTotalPrice = products.reduce(
+    (acc, product) => acc + product.totalPrice,
+    0
+  );
+  total.innerHTML = `$${myTotalPrice.toFixed(2)}`;
+};
 
-let number = document.querySelectorAll('.number');
+let number = document.querySelectorAll(".number");
 cards.forEach((card, i) => {
   let add = card.querySelector("#increment");
   let remove = card.querySelector("#decrement");
@@ -100,24 +111,35 @@ cards.forEach((card, i) => {
   add.addEventListener("click", () => {
     products[i].quantity++;
     products[i].totalPrice = products[i].quantity * products[i].price;
-    amount.innerHTML = `${products[i].quantity}`;
+    amount.innerHTML = products[i].quantity;
+    updateTotal();
     upadateOrder(i);
+    noItem.style.display = "none";
+    orderMade.style.display = "block";
+    myOrder.forEach((order, i) => {
+      let deleteButton = order.querySelector(".delete-icon");
+      deleteButton.addEventListener("click", () => {
+       console.log(order);
+       console.log(i);
+      });
+    });
   });
 
   remove.addEventListener("click", () => {
     if (products[i].quantity <= 0) {
       amount.innerHTML = 0;
-      
+      removeOrder();
     } else {
       products[i].quantity--;
       products[i].totalPrice = products[i].quantity * products[i].price;
       amount.innerHTML = `${products[i].quantity}`;
-      upadateOrder(i)
+      upadateOrder(i);
+      updateTotal();
     }
   });
 });
 
-const upadateOrder = (index)=>{
+const upadateOrder = (index) => {
   let order = {
     id: products[index].id,
     title: products[index].title,
@@ -125,53 +147,115 @@ const upadateOrder = (index)=>{
     price: products[index].price,
     description: products[index].description,
     totalPrice: products[index].totalPrice,
-    index: index
-  }
-  let existingOrder = orders.find(
-    (o) => o.id === order.id
-  );
-  if(existingOrder){
-    updateDetails(order)
-  }
-  else{
+    index: index,
+  };
+  let existingOrder = orders.find((o) => o.id === order.id);
+  if (existingOrder) {
+    updateDetails(order);
+  } else {
     addNewOrder(order);
     orders.push(order);
+    updateTotal()
+    
   }
-}
+};
 
-const addNewOrder = (order)=>{
-  orderWrapper.innerHTML += `<div class="order">
-  <div class="details-wrapper">
+const addNewOrder = (order) => {
+  orderWrapper.innerHTML += `<div class="myorder" id="order${order.id}">
+  <div class="details-wrapper" >
     <div class="food">${order.title}</div>
     <div class="details">
       <div class="number">x${order.quantity}</div>
-      <div class="price">$${order.price}</div>
+      <div class="price">$${order.price.toFixed(2)}</div>
       <div class="total-price">$${order.totalPrice.toFixed(2)}</div>
     </div>
   </div>
-  <div class="delete-icon">
+  <div class="delete-icon" id=deleteMe>
     <img src="assets/images/icon-remove-item.svg" alt="" />
   </div>
 </div>`;
-}
+deleteOrder(order)
+};
 
-const updateDetails = (order) =>{
-  let orderElement = orderWrapper.querySelector(
-    `.order:nth-child(${order.id + 1})`
-  );
-  orderElement.querySelector(".number").innerHTML = `x${order.quantity}`;
-  orderElement.querySelector(
-    ".total-price"
-  ).innerHTML = `$${order.totalPrice.toFixed(2)}`;  
-}
-
-
-
-// deleteButton.forEach(
-//     (button)=>{
-//        button.addEventListener('click', (e)=>{
-//        e.parentElement.parentElement.remove()
-//        })
+// const updateDetails = (order) => {
+//   let orderElement = orderWrapper.querySelector(
+//     `.myorder:nth-child(${order.id + 1})`
+//   );
+//   orderElement.querySelector(".number").innerHTML = `x${order.quantity}`;
+//   orderElement.querySelector(
+//     ".total-price"
+//   ).innerHTML = `$${order.totalPrice.toFixed(2)}`;
+// };
+// const updateDetails = (order) => {
+//     let orderElement = orderWrapper.querySelectorAll(".myorder")[order.id];
+//     if (orderElement) {
+//       orderElement.querySelector(".number").innerHTML = `x${order.quantity}`;
+//       orderElement.querySelector(
+//         ".total-price"
+//       ).innerHTML = `$${order.totalPrice.toFixed(2)}`;
 //     }
-// )
+//   };
+const updateDetails = (order) => {
+  //   let orderElement = document.querySelector(
+  //     `.myorder:nth-child(${order.index + 1})`
+  //   );
 
+  let orderElement = document.querySelector(`#order${order.id}`);
+
+  // console.log(order.quantity);
+  // console.log(orderElement);
+  if (orderElement) {
+    orderElement.querySelector(".number").innerHTML = `x${order.quantity}`;
+    orderElement.querySelector(
+      ".total-price"
+    ).innerHTML = `$${order.totalPrice.toFixed(2)}`;
+  }
+  
+};
+// deleteButton.forEach((button) => {
+//   button.addEventListener("click", (e) => {
+//     e.parentElement.parentElement.remove();
+//   });
+// });
+
+// Delete Order
+
+// deleteButton.addEventListener("click", (e) => {
+//   e.parentElement.parentElement.remove();
+// });
+
+
+
+// let deleteButton = document.querySelector('#deleteMe');
+// console.log(deleteButton)
+// deleteButton.addEventListener("click", () => {
+//   console.log('hello');
+//   console.log('hi');
+//  });
+  
+// myOrder.forEach((order, i) => {
+//   let deleteButton = order.querySelector(".delete-icon");
+//   deleteButton.addEventListener("click", () => {
+//    console.log(order);
+//    console.log(i);
+//   });
+// });
+
+const deleteOrder = (order)=>{
+  let orderElement = document.querySelector(`#order${order.id}`);
+  let deleteButton = orderElement.querySelector('#deleteMe')
+  
+      deleteButton.addEventListener('click', ()=>{
+       deleteButton.parentElement.style.display = 'none'
+       console.log(order)
+        })
+   
+  
+  
+}
+
+submit.addEventListener("click", () => {
+  console.log('hello');
+  console.log('hi');
+  console.log(myOrder)
+ });
